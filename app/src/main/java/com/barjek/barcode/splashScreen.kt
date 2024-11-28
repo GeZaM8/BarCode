2db0ab8dc7a14047b3dbb7a4427644025a40ab25
zkymubarok678 <mubarokzaky678@gmail.com>
2024-11-28 14:19:28 +0700

    SS

---
+++ app/src/main/java/com/barjek/barcode/splashScreen.kt
@@ -0,0 +1,24 @@
+package com.barjek.barcode
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class splashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000L) // Delay 3 detik
+    }
+}
