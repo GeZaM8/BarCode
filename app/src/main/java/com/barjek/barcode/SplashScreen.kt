d16e3d7f915491a1113c9e5ffa8e3291312bbc0e
GezaM8 <sergeshehzaadm2@gmail.com>
2024-12-07 09:47:51 +0700

    delete main

--- app/src/main/java/com/barjek/barcode/SplashScreen.kt
+++ app/src/main/java/com/barjek/barcode/SplashScreen.kt
@@ -6,7 +6,6 @@ import android.os.Handler
 import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.barjek.barcode.activity.MainActivity
 import com.barjek.barcode.activity.StartActivity
 
 class SplashScreen : AppCompatActivity() {
