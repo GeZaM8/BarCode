87962c5d97f00434e74c81b6d284130b50597209
Xiacio <satriagans0506201@gmail.com>
2024-12-08 14:36:17 +0700

    Change to binding

--- app/src/main/java/com/barjek/barcode/activity/YourProfile.kt
+++ app/src/main/java/com/barjek/barcode/activity/YourProfile.kt
@@ -3,31 +3,20 @@ package com.barjek.barcode.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.barjek.barcode.R
 import com.barjek.barcode.database.DatabaseHelper
+import com.barjek.barcode.databinding.ActivityYourProfileBinding
 
 class YourProfile : AppCompatActivity() {
     private lateinit var dbHelper: DatabaseHelper
+    private lateinit var binding: ActivityYourProfileBinding
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_your_profile)
+        binding = ActivityYourProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         
         dbHelper = DatabaseHelper(this)
-
-        val tvName = findViewById<TextView>(R.id.tvName)
-        val tvClass = findViewById<TextView>(R.id.tvClass)
-        val tvNISN = findViewById<TextView>(R.id.tvNISN)
-        val tvNIS = findViewById<TextView>(R.id.tvNIS)
-        val btnBack = findViewById<ImageButton>(R.id.btnBack)
-        val btnLogout = findViewById<Button>(R.id.btnLogout)
-
 
         val sharedPref = getSharedPreferences("UserPref", Context.MODE_PRIVATE)
         val userEmail = sharedPref.getString("email", "") ?: ""
@@ -35,22 +24,18 @@ class YourProfile : AppCompatActivity() {
         val user = dbHelper.getUserByEmail(userEmail)
 
         user?.let {
-            tvName.text = it.nama
-            tvClass.text = "${it.kelas} ${it.jurusan}"
-            tvNISN.text = it.nisn
-            tvNIS.text = it.nis
+            binding.tvName.text = it.nama
+            binding.tvClass.text = "${it.kelas} ${it.jurusan}"
+            binding.tvNISN.text = it.nisn
+            binding.tvNIS.text = it.nis
         }
-        
-        // Handle button clicks
-        btnBack.setOnClickListener {
+
+        binding.btnBack.setOnClickListener {
             finish()
         }
         
-        btnLogout.setOnClickListener {
-            // Clear SharedPreferences
+        binding.btnLogout.setOnClickListener {
             sharedPref.edit().clear().apply()
-            
-            // Redirect ke Login
             startActivity(Intent(this, LoginActivity::class.java))
             finishAffinity()
         }
